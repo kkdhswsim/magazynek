b7fa6ffd459f305a792a0f0ca1dbe288624f18ad
Krzysiek <krzysztof65@o2.pl>
2015-05-30 12:16:34 +0200

    Pierwszy commit Krzysiek

--- src/magazynek/Login.java
+++ src/magazynek/Login.java
@@ -12,11 +12,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author Dawid
- */
+//Mój komentarz
 public class Login extends javax.swing.JDialog {
     
     Connection conn = new connect().connect();
